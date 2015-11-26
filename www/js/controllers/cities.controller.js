2970011c8b567a7c06baf0024256f9e2e405ba31
vfdev <vfdev.5@gmail.com>
2015-11-26 17:23:01 +0100

    - change list items - refactor city controller with its service

--- www/js/controllers/cities.controller.js
+++ www/js/controllers/cities.controller.js
@@ -1,58 +1,100 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('starter.controllers.cities', [])
+        .controller('CityCtrl', CityController);
+
+    CityController.$inject = ['$scope', '$http', '$ionicPopup', 'Cities'];
+
+    function CityController($scope, $http, $ionicPopup, Cities) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+
+        var vm = this;
+
+        // Listen to cities.service
+        $scope.$on('sendAllCities', setup_all_cities)
+
+        // Define scope functions:
+        $scope.showAlert = show_alert;
+        $scope.searchCity = search_city;
+        $scope.itemButtons = [
+            {
+                text: 'Delete',
+                type: 'button-assertive',
+                onTap: function(item) {
+                    $scope.removeItem(item);
+                }
+            },
+            {
+                text: 'Edit',
+                type: 'button-calm',
+                onTap: function(item) {
+                    $scope.showEditItem(item);
+                }
+            }
+          ];
+
+        // Default actions:
+        request_all_cities();
+
+        // Define local functions:
 
 
-angular.module('starter.controllers.cities', [])
+        function show_alert(title, text) {
+            $ionicPopup.alert({
+                title: title,
+                template: text
+            });
+        };
 
-.controller('CityCtrl', function($scope, $http, $ionicPopup, Cities) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.showAlert = function(title, text) {
-    $ionicPopup.alert({
-      title: title,
-      template: text
-    });
-  };
-
-  $scope.cities = Cities.all();
-
-  // Search
-  $scope.searchCity = function(query) {
-
-    $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + query.city_name + '&APPID=8eb6e9b0f16cd2d2504e1bd6ea1e707a')
-    .success(function(data, status, headers, config) {
-
-      // check the 'cod' : Error if "cod":"404"
-      console.log("Data.cod : " + data.cod);
-
-      if (data.cod === 200) {
-        console.log("Data.id : " + data.id);
-        console.log("Data.name : " + data.name);
-
-        if (Cities.get(data.id) == null) {
-          Cities.set(data.id, data.name, "a beautiful city", 'https://upload.wikimedia.org/wikipedia/en/b/b9/Cool_City_Neighborhood_Logo.png');
-
-          $scope.cities = Cities.all();
-
-        } else {
-          console.log("City is already in the list");
+        function request_all_cities() {
+            console.log("Request all cities");
+            Cities.request_all();
         }
-      } else {
-        $scope.showAlert('La ville n\'est pas trouvé',
-          'Malheureusement, la ville \'' + query.city_name +  '\' n\'est pas trouvé dans le service météo.');
-      }
 
-    })
-    .error(function(data, status, headers, config) {
-      console.log("Data : " + data);
-      console.log("Status : " + status);
-      $scope.showAlert('Erreur de l\'application', 'Nous sommes désolé');
-    });
+        function setup_all_cities(foo, cities) {
+            console.log("Setup all cities : " + cities);
+            $scope.cities = cities;
+        }
 
-  };
-});
+        function search_city(query) {
 
+            $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + query.city_name + '&APPID=8eb6e9b0f16cd2d2504e1bd6ea1e707a')
+                .success(function(data, status, headers, config) {
+
+                    // check the 'cod' : Error if "cod":"404"
+                    console.log("Data.cod : " + data.cod);
+
+                    if (data.cod === 200) {
+                        console.log("Data.id : " + data.id);
+                        console.log("Data.name : " + data.name);
+
+                        if (Cities.get(data.id) == null) {
+                            Cities.set(data.id, data.name, "a beautiful city", 'https://upload.wikimedia.org/wikipedia/en/b/b9/Cool_City_Neighborhood_Logo.png');
+                            request_all_cities();
+                        } else {
+                            console.log("City is already in the list");
+                        }
+                    } else {
+                        $scope.showAlert('La ville n\'est pas trouvé',
+                        'Malheureusement, la ville \'' + query.city_name +  '\' n\'est pas trouvé dans le service météo.');
+                    }
+
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("Data : " + data);
+                    console.log("Status : " + status);
+                    $scope.showAlert('Erreur de l\'application', 'Nous sommes désolé');
+                });
+
+        };
+    };
+
+})();
--- www/js/services/cities.service.js
+++ www/js/services/cities.service.js
@@ -26,7 +26,7 @@
         console.log('CitiesService init');
 
         var service = {
-            all: get_all_cities,
+            request_all: request_all,
             get: get_city,
             set: set_city,
         };
@@ -38,12 +38,10 @@
 
         /////////
 
-        $localForage.getItem('cities').then(function(data) {
-        console.log("Get data from local forage : " + data);
-        cities = data;
-
-
-        });
+//        $localForage.getItem('cities').then(function(data) {
+//            console.log("Get data from local forage : " + data);
+//            cities = data;
+//        });
 
         //  if (cities === null) {
         //    console.log("Initialize localforage storage at first launch");
@@ -68,37 +66,85 @@
         //    });
         //  }
 
-        function get_all_cities() {
-            return vm.cities;
+        function request_all() {
+
+            if (vm.cities == null) {
+                $localForage.getItem('cities').then(function(data) {
+                    console.log("Get data from local forage : " + data);
+
+                    if (data == null) {
+                        console.log("Initialize localforage storage at first launch");
+                        // initialize localforage storage at first launch:
+                        data = {
+                            2972315:
+                            {
+                                id: 2972315,
+                                name: 'Toulouse',
+                                desc: 'Berceau des Airbus',
+                                logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Blason_ville_fr_Toulouse_%28Haute-Garonne%29.svg/73px-Blason_ville_fr_Toulouse_%28Haute-Garonne%29.svg.png'
+                            },
+                            524901:
+                            {
+                                id: 524901,
+                                name: 'Москва',
+                                desc: 'Столица нашей Родины',
+                                logo: 'http://upload.wikimedia.org/wikipedia/commons/d/da/Coat_of_Arms_of_Moscow.png'
+                            }
+                        };
+
+                        $localForage.setItem(
+                            'cities',
+                            data,
+                            function(err, result) {
+                                alert("Failed to store cities in the local storage");
+                            }
+                        );
+                        console.log("Initialized data : " + data);
+                    }
+                    vm.cities = data;
+                    var response = dict_to_array(vm.cities);
+                    $rootScope.$broadcast('sendAllCities', response);
+                });
+
+            } else {
+                var response = dict_to_array(vm.cities);
+                $rootScope.$broadcast('sendAllCities', response);
+            }
+        }
+
+        function dict_to_array(dict) {
+            var response = [];
+            for (var key in dict) {
+                response.push(dict[key]);
+            }
+            return response;
         }
 
         function get_city(id) {
-        for (var i = 0; i < cities.length; i++) {
-        if (cities[i].id === parseInt(id)) {
-        return cities[i];
-        }
-        }
-        return null;
+            var c = vm.cities[id];
+            return (typeof c === "undefined") ? null : c;
         }
 
         function set_city(id, name, desc, logo) {
-        var new_city = {
-        id: parseInt(id),
-        name: name,
-        desc: desc,
-        logo: logo
-        };
+            var new_city = {
+                id: parseInt(id),
+                name: name,
+                desc: desc,
+                logo: logo
+            };
 
-        cities.push(new_city);
+            vm.cities[new_city.id] = new_city;
 
-        $localForage.setItem(
-        'cities',
-        cities,
-        function(err, result) {
-        alert("Failed to store cities in the local storage");
-        });
-
+            $localForage.setItem(
+                'cities',
+                vm.cities,
+                function(err, result) {
+                    alert("Failed to store cities in the local storage");
+                }
+            );
         }
+
+
     };
 
 
